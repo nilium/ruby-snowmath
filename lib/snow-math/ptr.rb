4ffdaed4be5910463a3655eed73b5a96188d7da6
Noel Cower <ncower@gmail.com>
2013-07-02 23:58:45 -0700

    Fix snow-math/ptr includes

--- lib/snow-math/ptr.rb
+++ lib/snow-math/ptr.rb
@@ -15,11 +15,24 @@ module Snow::MathPointers
 
 end
 
-Snow::Vec3.include(Snow::MathPointers)
-Snow::Vec4.include(Snow::MathPointers)
-Snow::Quat.include(Snow::MathPointers)
-Snow::Mat4.include(Snow::MathPointers)
-Snow::Vec3Array.include(Snow::MathPointers) if Snow.const_defined?(:Vec3Array)
-Snow::Vec4Array.include(Snow::MathPointers) if Snow.const_defined?(:Vec4Array)
-Snow::QuatArray.include(Snow::MathPointers) if Snow.const_defined?(:QuatArray)
-Snow::Mat4Array.include(Snow::MathPointers) if Snow.const_defined?(:Mat4Array)
+class Snow::Vec3 ; include Snow::MathPointers ; end
+class Snow::Vec4 ; include Snow::MathPointers ; end
+class Snow::Quat ; include Snow::MathPointers ; end
+class Snow::Mat4 ; include Snow::MathPointers ; end
+
+if Snow.const_defined?(:Vec3Array)
+  class Snow::Vec3Array ; include Snow::MathPointers ; end
+end
+
+if Snow.const_defined?(:Vec4Array)
+  class Snow::Vec4Array ; include Snow::MathPointers ; end
+end
+
+if Snow.const_defined?(:QuatArray)
+  class Snow::QuatArray ; include Snow::MathPointers ; end
+end
+
+if Snow.const_defined?(:Mat4Array)
+  class Snow::Mat4Array ; include Snow::MathPointers ; end
+end
+
