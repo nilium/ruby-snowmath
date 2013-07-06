3de46deb490c65ea3e7488473341f1b4f698942b
Noel Cower <ncower@gmail.com>
2013-07-05 19:31:55 -0700

    Account for loss of cache when calling ___Array#resize!
    I might also add some sort of call to cached objects to allow them to
    update their pointers, but for now, this will do.
    
    I should probably move the caching code and such to C, but being able to
    handle it in Ruby is fairly convenient. I'd prefer to only drop down to
    C for things that would be called fairly often, and resizing a typed
    array shouldn't be one of those things.

--- lib/snow-math/array_cache.rb
+++ lib/snow-math/array_cache.rb
@@ -7,6 +7,7 @@ module Snow
     if const_defined?(klass_sym)
       const_get(klass_sym).class_exec {
         alias_method :__fetch__, :fetch
+        alias_method :__resize__!, :resize!
         alias_method :__array_cache_initialize__, :initialize
 
         def initialize(*args)
@@ -19,6 +20,21 @@ module Snow
         end
 
         alias_method :[], :fetch
+
+        def resize!(new_length)
+          @__cache__ = []
+          __resize__!(new_length)
+        end
+
+        def resize(new_length)
+          arr = self.class.new(new_length)
+          self_length = self.length
+          (0 ... (self_length < new_length ? self_length : new_length)).each {
+            |index|
+            arr.store(index, self.fetch(index))
+          }
+          return arr
+        end
       }
     end
   }
