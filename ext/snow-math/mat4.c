d8520b82950a88b232658b59a913a561a89aba74
Noel Cower <ncower@gmail.com>
2013-07-06 17:08:30 -0700

    Unroll loop in mat4_copy.

--- ext/snow-math/mat4.c
+++ ext/snow-math/mat4.c
@@ -42,10 +42,22 @@ void mat4_identity(mat4_t out)
 
 void mat4_copy(const mat4_t in, mat4_t out)
 {
-  if (in != out) {
-    int index = 0;
-    for (; index < 16; ++index) out[index] = in[index];
-  }
+  out[0 ] = in[0 ];
+  out[1 ] = in[1 ];
+  out[2 ] = in[2 ];
+  out[3 ] = in[3 ];
+  out[4 ] = in[4 ];
+  out[5 ] = in[5 ];
+  out[6 ] = in[6 ];
+  out[7 ] = in[7 ];
+  out[8 ] = in[8 ];
+  out[9 ] = in[9 ];
+  out[10] = in[10];
+  out[11] = in[11];
+  out[12] = in[12];
+  out[13] = in[13];
+  out[14] = in[14];
+  out[15] = in[15];
 }
 
 void mat4_set(
